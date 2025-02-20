9b3b59820ebd649f7d34d29ff827415d282f210b
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-02-21 00:17:27 +0600

    global  middleware added

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import subscriptionRouter from "./routes/subscription.router.js";
 import connectToDatabase from "./Database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
+
 const port = 5000 | PORT;
 
 const app = express();
-
+app.use(express.json)
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscription", subscriptionRouter);
-
+app.use(errorMiddleware)
 app.get("/", (req, res) => {
   res.send("Welcome to Robust Backend Server");
 });
